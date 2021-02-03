f91271edbc0283be9552f9155bed478245186e4b
medievalghoul <yeet7976@gmail.com>
2021-02-03 03:02:10 -0500

    [DRIVERS] Added serialport, vga driver

---
+++ drivers/serial_port.c
@@ -0,0 +1,64 @@
+#include <stddef.h>
+#include <string.h>
+#include <io/io_port.h>
+#include <io/serial_port.h>
+
+#define COM1	0x3F8
+#define COM2	0x2F8
+#define COM3	0x3E8
+#define COM4	0x2E8
+
+static int init_serial()
+{
+  outb(COM1 + 1, 0x00);    // Disable all interrupts
+  outb(COM1 + 3, 0x80);    // Enable DLAB (set baud rate divisor)
+  outb(COM1 + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
+  outb(COM1 + 1, 0x00);    //                  (hi byte)
+  outb(COM1 + 3, 0x03);    // 8 bits, no parity, one stop bit
+  outb(COM1 + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
+  outb(COM1 + 4, 0x0B);    // IRQs enabled, RTS/DSR set
+  outb(COM1 + 4, 0x1E);    // Set in loopback mode, test the serial chip
+  outb(COM1 + 0, 0xAE);    // Test serial chip (send byte 0xAE and check if serial returns same byte)
+
+  // Check if serial is faulty (i.e: not same byte as sent)
+  if(inb(COM1 + 0) != 0xAE) {
+    return 1;
+  }
+
+  // If serial is not faulty set it in normal operation mode
+  // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
+  outb(COM1 + 4, 0x0F);
+  return 0;
+}
+
+int serial_received() 
+{
+  return inb(COM1 + 5) & 1;
+}
+
+char read_serial() 
+{
+  while (serial_received() == 0);
+
+  return inb(COM1);
+}
+
+int is_transmit_empty() 
+{
+  return inb(COM1 + 5) & 0x20;
+}
+
+void write_serial(char a) 
+{
+  while (is_transmit_empty() == 0);
+
+  outb(COM1,a);
+}
+
+void serial_writeS(const char* buffer)
+{
+  init_serial();
+  for(uint32_t i = 0; i < strlen(buffer); i++) {
+    write_serial(buffer[i]);
+  }
+}
---
+++ drivers/vga.c
@@ -0,0 +1,88 @@
+#include <kernel/tty.h>
+#include <kernel/vga.h>
+#include <io/io_port.h>
+#include <stdint.h>
+#include <string.h>
+
+// Make a pixel(2 bytes), background is always black, a is a char, b is foreground color
+#define PAINT(a,b) (((b & 0xF) << 8) | (a & 0xFF))
+
+// Get pixel
+#define PIXEL(x, y) FRAMEBUFFER_MEMORY[y * 80 + x]
+
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+
+static size_t terminal_row = 0;    // x
+static size_t terminal_column = 0; // y
+static uint8_t terminal_color = 0;
+
+void terminal_initialize()
+{
+  terminal_color = vga_entry_color(VGA_WHITE, VGA_BLACK);
+  for(size_t y = 0; y < VGA_HEIGHT; y++) {
+    for(size_t x = 0; x < VGA_WIDTH; x++) {
+      const size_t index = y * VGA_WIDTH + x;
+      FRAMEBUFFER_MEMORY[index] = vga_entry(' ', terminal_color);
+    }
+  }
+}
+
+void terminal_setcolor(uint8_t color) { terminal_color = color; }
+
+void terminal_writeS(const char* data)
+{
+  while(*data != '\0') { terminal_putchar(*data); data++; }
+}
+
+void terminal_putchar(char c) 
+{
+  terminal_scroll();
+  if(c == '\n') {
+    terminal_row = 0;
+    terminal_column++;
+  }
+  else if(c == '\t') {
+      int i;
+      for(i = 0; i < 4; i++)
+        terminal_putchar(' ');
+  }
+  else if(c >= ' ') {
+      PIXEL(terminal_row, terminal_column) = PAINT(c, terminal_color);
+      terminal_row++;
+      if(terminal_row == 80) {
+        terminal_row = 0;
+        terminal_column++;
+      }
+  }
+    move_cursor();
+}
+
+void terminal_scroll() {
+    // Move up
+    void * start = (void*)FRAMEBUFFER_MEMORY + 1 * VGA_WIDTH * 2;
+    uint32_t size = terminal_column * VGA_WIDTH * 2;
+    if(terminal_column < 25) { return; }
+    memcpy(FRAMEBUFFER_MEMORY, start, size);
+
+    // Delete
+    start = (void*)FRAMEBUFFER_MEMORY + size;
+    memset(start, PAINT(0x20, terminal_color), VGA_WIDTH);
+    terminal_column--;
+}
+
+
+
+void move_cursor() 
+{
+  unsigned curr_pos = terminal_column * VGA_WIDTH + terminal_row;
+  outb(0x3D4, 14);
+  outb(0x3D5, curr_pos >> 8);
+  outb(0x3D4, 15);
+  outb(0x3D5, curr_pos);
+}
+
+
+
+
