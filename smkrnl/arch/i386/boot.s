493ba188d98055b6c72060a5429fb440d89a6d95
medievalghoul <yeet7976@gmail.com>
2021-02-18 00:34:00 -0500

    [KERNEL] Moved terminal_init to execute in bootlaoder

--- smkrnl/arch/i386/boot.s
+++ smkrnl/arch/i386/boot.s
@@ -6,7 +6,12 @@ MAGIC   equ 0x1BADB002
 CHECKSUM equ -(MAGIC + FLAGS)   ; checksum of above, to prove we are multiboot
 
 ; Early extern
+extern terminal_initialize
+extern _simple_banner
+extern _end_banner
 extern gdt_install
+extern idt_install
+extern kmain
 
 ; Setting the standards for the multiboot header to mark this
 ; as the kernel.
@@ -46,25 +51,24 @@ _start:
 	; in assembly as languages such as C cannot function without a stack.
 	mov esp, stack_top
 
+        ; Setting up the Framebuffer and small banner
+        call terminal_initialize
+        call _simple_banner
+
  	; This is a good place to initialize crucial processor state before the
-	; high-level kernel is entered. It's best to minimize the early
-	; environment where crucial features are offline. Note that the
-	; processor is not fully initialized yet: Features such as floating
-	; point instructions and instruction set extensions are not initialized
-	; yet. The GDT should be loaded here. Paging should be enabled here.
+	; high-level kernel is entered.The GDT should be loaded here. Paging should
+        ; be enabled here.
 	; C++ features such as global constructors and exceptions will require
 	; runtime support to work as well.
         call gdt_install
+        call idt_install
+
+        ; End of banner
+        call _end_banner
 
 	; Enter the high-level kernel. The ABI requires the stack is 16-byte
 	; aligned at the time of the call instruction (which afterwards pushes
-	; the return pointer of size 4 bytes). The stack was originally 16-byte
-	; aligned above and we've since pushed a multiple of 16 bytes to the
-	; stack since (pushed 0 bytes so far) and the alignment is thus
-	; preserved and the call is well defined.
-        ; note, that if you are building on Windows, C functions may have "_" 
-        ; prefix in assembly: _kernel_main
-	extern kmain
+	; the return pointer of size 4 bytes). 	extern kmain
 	call kmain
 
 	; If the system has nothing more to do, put the computer into an
