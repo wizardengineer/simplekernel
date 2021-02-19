6c9680d750020de529caa9e899825a17c368b939
medievalghoul <yeet7976@gmail.com>
2021-02-19 13:04:19 -0500

    Kernel Include: correctly fixed some macros

--- include/kernel/isr.h
+++ include/kernel/isr.h
@@ -15,10 +15,10 @@ typedef struct registers
 
 const char *exception_messages[IDT_FIRST_32_GATES];
 
-// Fault handler
+/* Fault handler */
 void fault_handler(regs registers);
 
-// You can find in exception_set.asm
+/* You can find in exception_set.asm */
 extern void isr0();
 extern void isr1();
 extern void isr2();
@@ -52,6 +52,20 @@ extern void isr29();
 extern void isr30();
 extern void isr31();
 
-
-
-
+/* Hardware Interrupts */
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
