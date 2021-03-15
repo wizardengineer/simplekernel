5e16d7ef4b5b48b2f336f3088371371c5d230019
medievalghoul <yeet7976@gmail.com>
2021-03-15 02:07:12 -0400

    [KERNEL/INCLUDE] Fixed Interrupt Descriptor Table (IDT)

--- include/kernel/idt.h
+++ include/kernel/idt.h
@@ -8,6 +8,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <kernel/printk.h>
+#include <static_assert.h>
 
 /* Entrys */
 typedef struct idt_entry
@@ -19,12 +20,18 @@ typedef struct idt_entry
   uint16_t base_high;
 } __attribute((packed)) idt_entry_t;
 
+//static_assert(sizeof(idt_entry) == 52,
+//              "IDT Gate fmt size does not match");
+
 /* Pointer to entry */
 typedef struct idt_ptr
 {
   uint16_t limit;
   uint32_t base;
 } __attribute((packed)) idt_ptr_t;
+//static_assert(sizeof(idt_ptr_t) == 48,
+//              "IDTR fmt size does not match");
+
 
 void idt_set_gate(uint8_t num, 
                   uint32_t base, 
--- include/kernel/isr.h
+++ include/kernel/isr.h
@@ -12,10 +12,28 @@
 
 typedef struct registers
 {
-  uint32_t gs, fs, es, ds;
-  uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
-  uint32_t int_no, err_code;
-  uint32_t eip, cs, eflags, useresp, ss;
+  uint32_t gs, 
+           fs, 
+           es, 
+           ds;
+
+  uint32_t edi, 
+           esi, 
+           ebp, 
+           esp, 
+           ebx, 
+           edx, 
+           ecx, 
+           eax;
+
+  uint32_t int_no, 
+           err_code;
+
+  uint32_t eip, 
+           cs, 
+           eflags, 
+           useresp, 
+           ss;
 } __attribute((packed)) regs, *p_regs;
 
 const char *exception_messages[IDT_FIRST_32_GATES];
